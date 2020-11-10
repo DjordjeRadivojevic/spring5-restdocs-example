8550a218abb4dc1eda223e8e4c5cc1e2ed452749
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-11-10 20:30:58 +0100

    documenting params and path variables

--- src/main/java/com/springframework/spring5restdocsexample/web/model/BeerDto.java
+++ src/main/java/com/springframework/spring5restdocsexample/web/model/BeerDto.java
@@ -1,12 +1,12 @@
 package com.springframework.spring5restdocsexample.web.model;
 
-import com.sun.istack.internal.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
